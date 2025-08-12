84ba38ea1fdc586b984ce3b8862231c3860f3e72
geovanysilvaa <lastzumbiezumbis@gmail.com>
2025-08-11 22:29:17 -0300

    mega atuazicao0-0

--- index.ts
+++ index.ts
@@ -18,22 +18,24 @@ this.emprestado = false;
 class Biblioteca{
 livros:Array<Livro>;
 cadastro:Array<string>;
+contador:number;
 constructor(){
 this.cadastro = [];
 this.livros = [];
+this.contador =0;
 }
 AdicionarLivro(){
 let menu = "📚 Adicionar Livros:\n";
-let titulo = prompt(menu+'Digite um titulo:');
-let categoria = prompt(menu+'Digite uma categoria:');
+let titulo = prompt(menu+'Digite um titulo:\n\n Preecher somente com nome!');
+let categoria = prompt(menu+'Digite uma categoria:\n\n Preecher somente com nome!');
 let ano1 = prompt(menu+'Digite ano:\n\n Preecher somente com numero!');
-let autor = prompt(menu+ 'Digite um autor:');
+let autor = prompt(menu+ 'Digite um autor:\n\n Preecher somente com nome!');
 if(!titulo || !categoria ||  !ano1 || !autor){
-alert('Preecha todos os campos para adicionar livros');
+alert('⚠ Preecha todos os campos para adicionar livros');
 }else{
 let ano = Number(ano1);
 if(isNaN(ano)){
-alert('Ano invalido');
+alert('⚠ Ano invalido');
 return;
 }
 let jaexiste:boolean;
@@ -44,7 +46,7 @@ jaexiste = true;
 }
 }
 if(jaexiste){
-alert('Livro ja adicionado');
+alert('⚠ Livro ja adicionado');
 }else{
 let novo = new Livro(titulo,categoria,ano,autor);
 this.livros.push(novo);
@@ -65,7 +67,7 @@ alert(lista);
 }
 RemoverLivros(){
 if(this.livros.length == 0){//verifica se tem livros adicionado no array livros 
-alert('Nenhum livro adicionado para remover');//se nao tiver ele emprime essa memsagem
+alert('⚠ Nenhum livro adicionado para remover');//se nao tiver ele emprime essa memsagem
 }else{
 let lista = '📚 Lista: \n';
 for(let i = 0; i < this.livros.length; i++){
@@ -77,7 +79,7 @@ if(resposta >= 1 && resposta <= this.livros.length){//so permite resposta >= 1 <
 this.livros.splice(resposta - 1, 1);//remove o livro na pos certa ex:(1 - 1,1); = pos 1 - 1 = 0; remove um elemento
 alert('Livro removido com sucesso');
 }else{
-alert('Opção invalida');
+alert('⚠ Opção invalida');
 }
 }
 }
@@ -85,7 +87,7 @@ Buscarlivros(){
 let resposta:string;
 resposta = String(prompt('Pesquisar livros: \n'));
 if(resposta == null || resposta == ""){
-alert('Operacao cancelada');
+alert('⚠ Operacao cancelada');
 return;
 }
 let jaexiste:boolean;
@@ -108,14 +110,25 @@ alert(resultado);
 }
 }
 Cadastrousuario(){
+if(this.contador >= 1){
+let resposta:number;
+resposta = Number(prompt('Voce esta bloqueado de fazer emprestimo:'+(this.contador)+'\n1->Pagar multa\n\nDigite "1" para pagar multa!'));
+if(resposta ==1){
+this.contador = 0;
+alert('Multa paga com sucesso, acesso liberado!');
+}else{
+alert('Opção invalido');
+return;
+}
+}
 let resposta: number;
 resposta = Number(prompt('Menu:\n1-> Cadastrar novo\n2-> Login'));
 // CADASTRO
 if(resposta == 1){
 let nome: string = String(prompt('Cadastro:\n-> Nome:'));
 let senha: string = String(prompt('Cadastro:\n-> Senha:'));
-if(!nome || !senha) {
-alert('Preencha todos os campos para cadastrar a conta');
+if(!nome || !senha){
+alert('⚠ Preencha todos os campos para cadastrar a conta');
 return;
 }
 let jaexiste: boolean = false;
@@ -125,7 +138,7 @@ jaexiste = true;
 }
 }
 if(jaexiste){
-alert('Conta já cadastrada, faça o login direto');
+alert('⚠ Conta já cadastrada, faça o login direto');
 }else{
 this.cadastro.push(nome,senha);
 alert('Conta cadastrada com sucesso');
@@ -135,7 +148,7 @@ if(resposta == 2){
 let nome1: string = String(prompt('Login:\n-> Digite seu usuário'));
 let senha1: string = String(prompt('Login:\n-> Digite sua senha'));
 if(!nome1 || !senha1){
-alert('Operação cancelada');
+alert('⚠ Operação cancelada');
 return;
 }
 let jaexiste:boolean;
@@ -146,7 +159,7 @@ jaexiste = true;
 }
 }
 if(!jaexiste){
-alert('Senha ou usuário incorretos');
+alert('⚠ Senha ou usuário incorretos');
 }else{
 alert('Acesso liberado');
 let acao = Number(prompt("1-> Emprestar livro\n2-> Devolver livro"));
@@ -154,7 +167,7 @@ let acao = Number(prompt("1-> Emprestar livro\n2-> Devolver livro"));
 if(acao == 1){
 let disponiveis = this.livros.filter(l => !l.emprestado);//cria um novo array com os que passaram no teste = 
 if (disponiveis.length == 0) {
-alert("Nenhum livro disponível para empréstimo.");
+alert("⚠ Nenhum livro disponível para empréstimo.");
 return;
 }
 let lista = "Lista: livros disponíveis para empréstimo:\n\n";
@@ -167,7 +180,7 @@ disponiveis[escolha].emprestado = true;
 disponiveis[escolha].prazoDevolucao = Date.now() + 30 * 1000; // 30 segundos
 alert(`Livro emprestado! Prazo: 30 segundos.`);
 }else{
-alert("Livro inválido ou já emprestado.");
+alert("⚠Livro inválido ou já emprestado.");
 }
 }
 // DEVOLVER
@@ -181,7 +194,8 @@ let agora = Date.now();
 if (this.livros[i]?.prazoDevolucao !== undefined && agora <= this.livros[i]!.prazoDevolucao!){
 alert("Devolução no prazo! Nenhuma multa.");
 }else{
-alert("Atrasado! Multa aplicada.");
+alert("⚠ Atrasado! Multa aplicada.");
+this.contador = this.contador + 1;
 }
 this.livros[i]!.emprestado = false;
 this.livros[i]!.prazoDevolucao = undefined;
@@ -189,7 +203,7 @@ return;
 }
 }
 if(!encontrado){
-alert("Livro não encontrado ou não está emprestado.");
+alert("⚠ Livro não encontrado ou não está emprestado.");
 }
 }
 }
@@ -202,10 +216,10 @@ let minhaBiblioteca = new Biblioteca();//cria o biblioteca
 //retornar a opção (0-> sair, 1->func 01, 2->func 02, 3->func 03...)
 function acessar():number{
 let resposta:number;
-resposta = Number(prompt('|-------------------------------BIBLIOTECA----------------------------------|Menu:\n1-> Adicionar Livros  \n2📚-> Lista Livros \n3🚮-> Excluir Livros \n4-> Buscar livros\n5-> Emprestimo de livros\n6-> Sair'));
+resposta = Number(prompt('|-------------------------------BIBLIOTECA----------------------------------|Menu:\n1-> Adicionar Livros\n2📚-> Lista Livros\n3🚮-> Excluir Livros\n4-> Buscar livros\n5-> Emprestimo de livros\n6-> Sair'));
 while (isNaN(resposta) || resposta < 0 || resposta > 7) {
 alert('opção inválida');
-resposta = Number(prompt('Menu:\n1-> Adicionar Livros  \n2📚-> Lista Livros \n3🚮-> Excluir Livros \n4-> Buscar livros\n5-> Emprestimo de livros\n6-> Sair'));
+resposta = Number(prompt('Menu:\n1-> Adicionar Livros\n2📚-> Lista Livros\n3🚮-> Excluir Livros \n4-> Buscar livros\n5-> Emprestimo de livros\n6-> Sair'));
 }
 return resposta;
 }
