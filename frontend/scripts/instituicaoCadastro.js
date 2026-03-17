666499ff0b89d882a21063757b6f23d728d7c79b
Geovanysilvaa <lastzumbiezumbis@gmail.com>
2026-03-17 12:13:09 -0300

    feat:add logica nas tela login e cadastro + validação

--- frontend/scripts/instituicaoCadastro.js
+++ frontend/scripts/instituicaoCadastro.js
@@ -2,13 +2,13 @@
 import { cadastroIntituicao } from "../api/instituicaoCadastro.js";
 
 /* Botões */
-
-const btn_cadastro = document.querySelector("#btn_cadastro");
-console.log(btn_cadastro)
+const formulario_cadastro = document.querySelector("#formulario_cadastro");
 
 /* Evento */
 
-btn_cadastro.addEventListener("click", () => {
+formulario_cadastro.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+
     const nome = document.querySelector("#inome").value;
     const email = document.querySelector("#iemail").value;
     const senha = document.querySelector("#isenha").value;
